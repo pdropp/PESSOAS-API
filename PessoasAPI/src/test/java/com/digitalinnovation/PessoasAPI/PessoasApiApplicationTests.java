47b8d842e43dc6a601f25671491bce7b1dbfc26c
Pedro Paulo <pedrogks17@gmail.com>
2021-07-01 21:31:21 -0300

    Pedro

---
+++ PessoasAPI/src/test/java/com/digitalinnovation/PessoasAPI/PessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.PessoasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
